8f69da4784184bd2b87eee04351fb22c6cb0fb58
风 <skycreat@gmail.com>
2024-12-24 22:46:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,10 +18,10 @@ sed -i "/helloworld/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
 echo '=========Add SSR-Plus source OK!========='
 
-echo '添加passwall软件源'
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo '=========Add passwall source OK!========='
+# echo '添加passwall软件源'
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+# echo '=========Add passwall source OK!========='
 
 echo '添加AdguardHome'
 # rm -rf feeds/kenzo/adguardhome
@@ -42,22 +42,22 @@ rm -rf package/lean/k3screenctrl
 git clone https://github.com/JE668/k3screenctrl_build.git package/lean/k3screenctrl/
 echo '=========Replace k3screen drive plug OK!========='
 
-echo '添加OpenClash'
-rm -rf package/lean/luci-app-openclash
-mkdir package/luci-app-openclash
-cd package/luci-app-openclash
-git init
-git remote add -f origin https://github.com/vernesong/OpenClash.git
-git config core.sparsecheckout true
-echo "luci-app-openclash" >> .git/info/sparse-checkout
-git pull --depth 1 origin master
-git branch --set-upstream-to=origin/master master
+# echo '添加OpenClash'
+# rm -rf package/lean/luci-app-openclash
+# mkdir package/luci-app-openclash
+# cd package/luci-app-openclash
+# git init
+# git remote add -f origin https://github.com/vernesong/OpenClash.git
+# git config core.sparsecheckout true
+# echo "luci-app-openclash" >> .git/info/sparse-checkout
+# git pull --depth 1 origin master
+# git branch --set-upstream-to=origin/master master
 # git clone https://github.com/vernesong/OpenClash package/lean/luci-app-openclash
-echo '=========Add OpenClash source OK!========='
+# echo '=========Add OpenClash source OK!========='
 
-# echo '添加Haiibo软件源'
-# sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
-# echo '=========Add Haiibo source OK!========='
+ echo '添加Haiibo软件源'
+ sed -i '1i src-git haiibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
+ echo '=========Add Haiibo source OK!========='
 
 # echo '添加kenzok8 openwrt-package'
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
@@ -68,17 +68,17 @@ echo '=========Add OpenClash source OK!========='
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 # echo '=========Add kenzok8 source OK!========='
 
-# echo '添加MosDNS'
-# rm -rf feeds/packages/net/v2ray-geodata
-# rm -rf feeds/haiibo/luci-app-mosdns
-# rm -rf feeds/haiibo/mosdns
-# rm -rf package/feeds/haiibo/luci-app-mosdns
-# rm -rf package/feeds/haiibo/mosdns
-# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-# find ./ | grep Makefile | grep mosdns | xargs rm -f
-# git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-# git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-# echo '=========Add MosDNS OK!========='
+ echo '添加MosDNS'
+ rm -rf feeds/packages/net/v2ray-geodata
+ rm -rf feeds/haiibo/luci-app-mosdns
+ rm -rf feeds/haiibo/mosdns
+ rm -rf package/feeds/haiibo/luci-app-mosdns
+ rm -rf package/feeds/haiibo/mosdns
+ find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+ find ./ | grep Makefile | grep mosdns | xargs rm -f
+ git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+ echo '=========Add MosDNS OK!========='
 
 echo '移除bcm53xx中的其他机型'
 sed -i '421,453d' target/linux/bcm53xx/image/Makefile
